81025df1e3dd1075507313de62a5c225f06c9177
syednoor058 <syednoor058@gmail.com>
2024-09-09 13:39:29 +0600

    Cors credientials added.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
-import connectDB from './config/db.js';
-import { isAdmin, requireSignIn } from './middlewares/authMiddleware.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import { isAdmin, requireSignIn } from "./middlewares/authMiddleware.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
-
 
 dotenv.config();
 const app = express();
@@ -15,24 +14,22 @@ const app = express();
 connectDB();
 
 //Middlewares
-app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN, credentials: true }));
 app.use(express.json());
-app.use(morgan('dev'));
-
+app.use(morgan("dev"));
 
 // All routes
-app.use('/api', productRoute)
-app.use('/api', categoryRoute)
-app.use('/api/v1/auth', authRoute)
-app.get('/api', requireSignIn, isAdmin, (req, res) => {
-    res.send({
-        message: "Welcome to Kick-off backend."
-    })
-})
+app.use("/api", productRoute);
+app.use("/api", categoryRoute);
+app.use("/api/v1/auth", authRoute);
+app.get("/api", requireSignIn, isAdmin, (req, res) => {
+  res.send({
+    message: "Welcome to Kick-off backend.",
+  });
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening at ${PORT}`);
-    
-})
+  console.log(`Server is listening at ${PORT}`);
+});
